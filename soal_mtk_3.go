24ad9ea6c6186763fb4296c366689a292ac5a8c9
ivnmhdka <ivanmahadika05@gmail.com>
2023-10-12 12:14:36 +0700

    mtk C

---
+++ soal_mtk_3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	var d1, d2, d3 int
+
+	fmt.Print("Masukkan bilangan bulat positif (kurang dari atau sama dengan 900) : ")
+	fmt.Scan(&x)
+
+	d1 = (x / 100)
+	fmt.Printf("Nilai dari d1 : %d", d1)
+
+	d2 = (x / 10) % 10
+	fmt.Printf(" Nilai dari d2 : %d", d2)
+
+	d3 = (x / 1) % 10
+	fmt.Printf(" Nilai dari d3 : %d", d3)
+}
