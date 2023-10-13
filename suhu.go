62735152cf8de85420002f330255a8265abe7af5
ivnmhdka <ivanmahadika05@gmail.com>
2023-10-13 13:07:51 +0700

    :

--- suhu.go
+++ suhu.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	var celcius, reamur, fahrenheit, kelvin float64
 
+	fmt.Print("Masukkan suhu : ")
+
 	fmt.Scan(&celcius)
 	reamur = celcius * 4.0 / 5.0
 	fahrenheit = celcius*9.0/5.0 + 32.0
